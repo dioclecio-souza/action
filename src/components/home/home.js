983aba43e24b7c28f228a8a1b49791b04d13a452
didithx <diocleciosouza10@gmail.com>
2023-03-20 10:58:40 -0300

    atualizaçoes site

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,25 +4,25 @@ import Jovem from '../../images/jovem-afro.png'
 import Light from '../../images/light.png'
 import Logo from '../../images/logo.png'
 import Numbers from '../../images/numbers.png'
+import { Button } from 'bootstrap'
 
 const home = () => {
   return (
 
       <div className="container">
         <div>
-          <img src={Logo} alt="logo" id="logo" />
-          <h1 className="home-title">Lorem Ipsum is simply <br></br>
-          dummy text of <span>the printing</span>
-          <br></br> and typesetting
-          </h1>
-        </div>
-        
-        
-        
-        
-        <div className="home-description">
-          <p>Industry. Lorem Ipsum has been the industry's
-          <br></br>standard dummy text ever since the.</p>
+            <div className="content-info">
+              <img src={Logo} alt="logo" id="logo" />
+              <h1 className="home-title">Lorem Ipsum is simply <br></br>
+              dummy text of <span>the printing</span>
+              <br></br> and typesetting
+              </h1>
+              <p className="home-description">Industry. Lorem Ipsum has been the industry's
+              <br></br>standard dummy text ever since the.</p>
+              <button id="button">
+                Saiba Mais
+              </button>
+            </div>
         </div>
       </div>
       
