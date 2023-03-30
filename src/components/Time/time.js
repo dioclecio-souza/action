88231d5b933212a2f0f6995e8d5271b65bacdb10
didithx <diocleciosouza10@gmail.com>
2023-03-30 11:21:01 -0300

    time, whois - terminados

--- src/components/Time/time.js
+++ src/components/Time/time.js
@@ -1,36 +1,63 @@
 import React from 'react'
-import './styles.css'
+import './time.css'
+import ImgPerson1 from '../../assets/homem1.png'
+import ImgPerson2 from '../../assets/homem2.png'
+import ImgPerson3 from '../../assets/homem3.png'
 
 const time = () => {
   return (
-    <div className="container text-center">
-        <h1> Nosso Time</h1>
-        <div className="row row-cols-3">
-            <div className="team-box">
-                <h3>DIEGO NOVAES</h3>
-                <p>Funçao Interna</p>
-            </div>
-            <div className="team-box">
-                <h3>DIEGO NOVAES</h3>
-                <p>Funçao Interna</p>
-            </div>
-            <div className="team-box">
-                <h3>DIEGO NOVAES</h3>
-                <p>Funçao Interna</p>
-            </div>
-            <div className="team-box">
-                <h3>DIEGO NOVAES</h3>
-                <p>Funçao Interna</p>
-            </div>
-            <div className="team-box">
-                <h3>DIEGO NOVAES</h3>
-                <p>Funçao Interna</p>
-            </div>
-            <div className="team-box">
-                <h3>DIEGO NOVAES</h3>
-                <p>Funçao Interna</p>
-            </div>
+    <div>
+        <h1 className="title-time">Nosso Time</h1>
+        <div className="box1">
+            <h2 className="title-box">DIEGO NOVAES</h2>
+            <p className="p-box">Função Interna</p>
         </div>
+        <div className="box2">
+            <h2 className="title-box">DIEGO NOVAES</h2>
+            <p className="p-box">Função Interna</p>
+        </div>
+        <div className="box3">
+            <h2 className="title-box">DIEGO NOVAES</h2>
+            <p className="p-box">Função Interna</p>
+        </div>
+        <div className="box4">
+            <h2 className="title-box">DIEGO NOVAES</h2>
+            <p className="p-box">Função Interna</p>
+        </div>
+        <div className="box5">
+            <h2 className="title-box">DIEGO NOVAES</h2>
+            <p className="p-box">Função Interna</p>
+        </div>
+        <div className="box6">
+            <h2 className="title-box">DIEGO NOVAES</h2>
+            <p className="p-box">Função Interna</p>
+        </div>
+
+        <div>
+            <img src={ImgPerson1} alt="homem1" className="img-person1"/>
+            <h2>DIEGO NOVAES</h2>
+            <p>Funçao Interna</p>
+
+            <img src={ImgPerson2} alt="homem2" className="img-person2"/>
+            <h2>DIEGO NOVAES</h2>
+            <p>Função Interna</p>
+
+            <img src={ImgPerson3} alt="homem3" className="img-person3"/>
+            
+
+            <img src={ImgPerson1} alt="homem1" className="img-person4"/>
+            <h2>DIEGO NOVAES</h2>
+            <p>Funçao Interna</p>
+
+            <img src={ImgPerson2} alt="homem2" className="img-person5"/>
+            <h2>DIEGO NOVAES</h2>
+            <p>Funçao Interna</p>
+
+            <img src={ImgPerson3} alt="homem3" className="img-person6"/>
+            <h2>DIEGO NOVAES</h2>
+            <p>Funçao Interna</p>
+        </div>
+
     </div>
   )
 }
--- src/components/WhoIsEntony/whoIsEntony.js
+++ src/components/WhoIsEntony/whoIsEntony.js
@@ -1,14 +1,25 @@
 import React from 'react'
-
+import './whoIs.css'
+import Jovem from '../../assets/jovem-camisa.png'
+import Entony from '../../assets/Entony.png'
+import LogoBorder from '../../assets/logo-border.png'
+import Light1 from '../../assets/light-whois1.png'
 
 
 const whoIsEntony = () => {
   return (
     <div className="container-whoIs">
-        <h1>Quem é <span>Entony?</span></h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium expedita autem 
-            dolorum possimus quibusdam tempore placeat rem dolorem maiores incidunt 
-            asperiores tenetur, itaque rerum odit, quod perferendis veritatis aliquam dignissimos!
+        <img src={Jovem} alt="jovem" id="jovem-afro" />
+        <img src={Entony} alt="assinatura" id="sign" />
+        <img src={LogoBorder} alt="logo" id="logo-border" />
+        <img src={Light1} alt="logo" id="light1" />
+        <h1 className="title-whoIs">Quem é <span>Entony?</span></h1>
+        <p className="desc-whoIs">Lorem Ipsum is simply dummy text of the printing<br></br>
+          and typesetting industry. Lorem Ipsum has been the<br></br>
+          industry's standard dummy text ever since the 1500s,<br></br>
+          Lorem Ipsum is simply dummy text of the printing<br></br>
+          and typesetting industry. Lorem Ipsum has been the<br></br>
+          industry's standard dummy text ever since the 1500s,
         </p>
     </div>
   )
