56c45b370ffbac47925e455d1d376b846590a670
didithx <diocleciosouza10@gmail.com>
2023-04-02 16:41:52 -0300

    finalizado

--- src/components/CommonQuestions/commonQuestions.js
+++ src/components/CommonQuestions/commonQuestions.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import FundoQuestions from '../../assets/fundo-questions.png'
 
 
 import './questions.css'
@@ -16,119 +16,38 @@ const commonQuestions = () => {
         <h1 className="title-Questions">Perguntas Frequentes</h1>
 
 
-        {/* Inicio  Accordion Perguntas Frequentes */}
+        {/* Inicio   Perguntas Frequentes */}
 
 
-        <div className="accordion" id="accordion">
-            <div className="accordion-item">
-                <h2 className="accordion-header" id="headingOne">
-                    <button className="accordion-button" type="button" 
-                    data-bs-toggle="collapse" data-bs-target="#collapseOne" 
-                    aria-expanded="false" aria-controls="collapseOne">
-                    Qual a diferença entre o plano anual e o mensal?
-                    </button>
-                </h2>
-                <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordion">
-                    <div className="accordion-body">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has <br></br>
-                    been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy <br></br>
-                    text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard <br></br>
-                    dummy text ever since the 1500s,
-                    </div>
-                </div>
-            </div>
+        <div className="boxQuestionsOne">
+            <h1 className="box-headerOne">Qual a diferença entre o plano Anual e Mensal?</h1>
+            <p className="questions-p">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has<br></br>
+            been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy<br></br>
+            text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard<br></br>
+            dummy text ever since the 1500s,</p>
         </div>
 
-        <br></br>
-
-        <div className="accordion" id="accordion">
-            <div className="accordion-item">
-                <h2 className="accordion-header" id="headingTwo">
-                    <button className="accordion-button" type="button" 
-                    data-bs-toggle="collapse" data-bs-target="#collapseTwo" 
-                    aria-expanded="false" aria-controls="collapseTwo">
-                    Qual a diferença entre o plano anual e o mensal?
-                    </button>
-                </h2>
-                <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordion">
-                    <div className="accordion-body">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has <br></br>
-                    been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy <br></br>
-                    text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard <br></br>
-                    dummy text ever since the 1500s,
-                    </div>
-                </div>
-            </div>
+        <div className="boxQuestionsTwo">
+            <h1 className="box-headerOne">Qual a diferença entre o plano Anual e Mensal?</h1>
         </div>
 
-        <br></br>
-
-        <div className="accordion" id="accordion">
-            <div className="accordion-item">
-                <h2 className="accordion-header" id="headingThree">
-                    <button className="accordion-button" type="button" 
-                    data-bs-toggle="collapse" data-bs-target="#collapseThree" 
-                    aria-expanded="false" aria-controls="collapseThree">
-                    Qual a diferença entre o plano anual e o mensal?
-                    </button>
-                </h2>
-                <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordion">
-                    <div className="accordion-body">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has <br></br>
-                    been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy <br></br>
-                    text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard <br></br>
-                    dummy text ever since the 1500s,
-                    </div>
-                </div>
-            </div>
+        <div className="boxQuestionsThree">
+            <h1 className="box-headerOne">Qual a diferença entre o plano Anual e Mensal?</h1>
         </div>
 
-        <br></br>
-
-        <div className="accordion" id="accordion">
-            <div className="accordion-item">
-                <h2 className="accordion-header" id="headingFor">
-                    <button className="accordion-button" type="button" 
-                    data-bs-toggle="collapse" data-bs-target="#collapseFor" 
-                    aria-expanded="false" aria-controls="collapseFor">
-                    Qual a diferença entre o plano anual e o mensal?
-                    </button>
-                </h2>
-                <div id="collapseFor" className="accordion-collapse collapse" aria-labelledby="headingFor" data-bs-parent="#accordion">
-                    <div className="accordion-body">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has <br></br>
-                    been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy <br></br>
-                    text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard <br></br>
-                    dummy text ever since the 1500s,
-                    </div>
-                </div>
-            </div>
+        <div className="boxQuestionsFor">
+            <h1 className="box-headerOne">Qual a diferença entre o plano Anual e Mensal?</h1>
         </div>
 
-        <br></br>
-
-        <div className="accordion" id="accordion">
-            <div className="accordion-item">
-                <h2 className="accordion-header" id="headingFive">
-                    <button className="accordion-button" type="button" 
-                    data-bs-toggle="collapse" data-bs-target="#collapseFive" 
-                    aria-expanded="false" aria-controls="collapseFive" >
-                    Qual a diferença entre o plano anual e o mensal?
-                    </button>
-                </h2>
-                <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordion">
-                    <div className="accordion-body">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has <br></br>
-                    been the industry's standard dummy text ever since the 1500s, Lorem Ipsum is simply dummy <br></br>
-                    text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard <br></br>
-                    dummy text ever since the 1500s,
-                    </div>
-                </div>
-            </div>
+        <div className="boxQuestionsFive">
+            <h1 className="box-headerOne">Qual a diferença entre o plano Anual e Mensal?</h1>
         </div>
 
 
-        {/* Final Accordion Perguntas Frequentes */}
+
+        <img src={FundoQuestions} alt="fundo" className="fundo-questions"/>
+
+        {/* Final  Perguntas Frequentes */}
         
         
     </div>
